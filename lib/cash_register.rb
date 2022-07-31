f241087f417db630a1ed501158eae1717cc24239
majaliju <majalijunyc@gmail.com>
2022-07-30 22:36:34 -0400

    commit tree 1963ca0046c6e209045a39e120c94ba7b4613533

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+class CashRegister
+  attr_reader :discount
+  attr_accessor :total
 
+  def initialize employee_discount = 0
+    @total = 0 
+    @discount = employee_discount
+  end
+
+  def add_item title, price, quantity=1
+    self.total += price & quantity
+  end
+
+  def apply_discount
+  end
+
+  def items
+  end
+
+  def void_last_transaction
+  end
+end
